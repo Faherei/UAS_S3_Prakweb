8d6c657be1f2e479b14a01597ade5016974ec4e5
Fahri Muhaimin <fahrimuhaimin132@gmail.com>
2024-12-14 19:04:19 +0700

    update profile mahasiswa

--- views/profile_mahasiswa.php
+++ views/profile_mahasiswa.php
@@ -18,26 +18,31 @@ $mhs = $_SESSION['nama'];
 </head>
 
 <body>
-
-    <div class="profilecontainer">
-
-        <div class="profile">
-            <?php
-                $select = mysqli_query($conn, "SELECT * FROM `mahasiswa` WHERE nama = '$mhs'");
-                if(mysqli_num_rows($select) > 0){
-                    $fetch = mysqli_fetch_assoc($select);
-                }
-                if($fetch['profile'] == ''){
-                    echo '<img src="../assets/uploads/default-profile.jpeg">';
-                }else{
-                    echo '<img src="../assets/uploaded/'.$fetch['profile'].'">';
-                }
+    <nav>aa</nav>
+    <main>
+        <div class="card">
+            <div class="profil">
+                <?php
+            $select = mysqli_query($conn, "SELECT * FROM `mahasiswa` WHERE nama = '$mhs'");
+            if(mysqli_num_rows($select) > 0){
+                $fetch = mysqli_fetch_assoc($select);
+            }
+            if($fetch['profile'] == ''){
+                echo '<img src="../assets/uploads/default-profile.jpeg">';
+            }else{
+                echo '<img src="../assets/uploaded/'.$fetch['profile'].'">';
+            }
             ?>
-            <h3><?php echo $fetch['nama']; ?></h3>
-            <a href="../views/update_pmahasiswa.php">Update Profile</a>
         </div>
-
+        <div class="card-body">
+            <h3><?php echo $fetch['nama']; ?></h3>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="../views/update_pmahasiswa.php">
+                <button type="submit" class="btn_submit">Change</button>
+            </a>
+            <button type="button" class="close btn_cancel">Back</button>
+        </div>
     </div>
-    
+</main>
 </body>
 </html>
